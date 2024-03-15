2747dc6a97f6b55c5d6364d4ddb808a1b4599882
Julia Djomina <julia.djomina@gmail.com>
2024-03-15 23:52:02 +0200

    Text -> Title and Number -> Amount change

--- filters_back/src/main/java/test/assignment/filters/persistence/repository/CriteriaRepository.java
+++ filters_back/src/main/java/test/assignment/filters/persistence/repository/CriteriaRepository.java
@@ -1,16 +1,8 @@
 package test.assignment.filters.persistence.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
-import test.assignment.filters.persistence.model.ComparisonOperator;
 import test.assignment.filters.persistence.model.criteria.Criteria;
-import test.assignment.filters.persistence.model.criteria.DateCriteria;
-import test.assignment.filters.persistence.model.criteria.NumberCriteria;
-import test.assignment.filters.persistence.model.criteria.TextCriteria;
-
-import java.util.List;
 
 @Repository
 public interface CriteriaRepository extends JpaRepository<Criteria, Long> {
